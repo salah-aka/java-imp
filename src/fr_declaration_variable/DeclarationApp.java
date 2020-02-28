e6ba0437eaacf29905bbf7e182afa08c58b621e1
Salaheddine EL Majdoub <salahmajdoub41@gmail.com>
2020-02-28 12:30:49 +0100

    TP1

---
+++ src/fr_declaration_variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr_declaration_variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte a = 12;
+		short b = 32000;
+		int c = 2_000_000_000;
+		long d = 90000000000000L;
+		float e = -0.5f;
+		double f = 1000000000000000000000000000000000D;
+		boolean g = true;
+		char h = 'A';
+		String chaine = "CouCou";
+		
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	System.out.println(h);
+	System.out.println(chaine);
+		
+	}
+
+}
