7c38a3727d908b4606d9148b5220546327b17ccb
Salaheddine EL Majdoub <salahmajdoub41@gmail.com>
2020-03-02 14:06:54 +0100

    Exo9 :FirstLast6

---
+++ src/fr/algorithmie/FirstLast6.java
@@ -0,0 +1,17 @@
+package fr.algorithmie;
+
+public class FirstLast6 {
+
+	public static void main(String[] args) {
+	int [] array = {1, 12, 17, 14, 5, -9, 0, 6};
+	boolean b;
+	if ( b = (array.length >= 1))
+		if (array[0] == 6 || array[array.length-1] == 6) {
+				System.out.println(b + " : le premier élément ou le dernier élément vaut 6");
+							}
+	else { 
+	        	System.out.println(b + "autre cas");
+		  }
+	}
+	}
+
